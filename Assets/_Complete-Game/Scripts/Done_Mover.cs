fdbe7ea77d5a76d9956ddaa4741a9bb79c905d17
fndsn <fndsn@users.noreply.github.com>
2020-08-13 19:53:38 +0300

    Initial commit

---
+++ Assets/_Complete-Game/Scripts/Done_Mover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Done_Mover : MonoBehaviour
+{
+	public float speed;
+
+	void Start ()
+	{
+		GetComponent<Rigidbody>().velocity = transform.forward * speed;
+	}
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Boundary"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
