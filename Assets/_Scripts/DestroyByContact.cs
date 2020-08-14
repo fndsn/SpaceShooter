64a198d96d62a774d2949e0125adb09f63c18834
fndsn <fndsn@users.noreply.github.com>
2020-08-14 07:04:54 +0300

    D2

---
+++ Assets/_Scripts/DestroyByContact.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosionEffect, playerExplosionEffect;
+    public int scoreValue;
+    GameController gameController;
+
+    private void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        else
+        {
+            Debug.Log("'GameController' bulunamadı.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boundary"))
+        {
+            return;
+        }
+        Instantiate(explosionEffect, transform.position, Quaternion.identity);
+        gameController.AddScore(scoreValue);
+
+
+
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(playerExplosionEffect, other.transform.position, Quaternion.identity);
+            //gameController.GameOver();
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using JetBrains.Annotations;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public float startWait;
+    public float spawnWait;
+    public float waveWait;
+    public int hazardCount;
+
+    public float spawnCounter;
+
+    public Text scoreText;
+    int score;
+    public GameObject gameOverPanel;
+    bool IsGameOver = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(SpawnWaves());
+        gameOverPanel.SetActive(false);
+        UpdateText();
+    }
+
+    private void Update()
+    {
+        if (IsGameOver)
+        {
+            return;
+        }
+        spawnCounter += Time.deltaTime;
+        if (spawnCounter >= spawnWait)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+            Instantiate(hazard, spawnPosition, Quaternion.identity);
+
+            spawnCounter = 0;
+        }
+    }
+
+    public void AddScore(int newScore)
+    {
+        score += newScore;
+        UpdateText();
+    }
+
+    
+
+    void UpdateText()
+    {
+        scoreText.text = "Score : " + score;
+    }
+
+    //IEnumerator SpawnWaves()
+    //{
+    //    // oyuncu hazırlık süresi
+    //    yield return new WaitForSeconds(startWait);
+
+    //    while (true)
+    //    {
+    //        for (int i = 0; i < hazardCount; i++)
+    //        {
+    //            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+    //            Instantiate(hazard, spawnPosition, Quaternion.identity);
+    //            yield return new WaitForSeconds(spawnWait);
+    //        }
+    //        yield return new WaitForSeconds(waveWait);
+    //    }
+    //}
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+        IsGameOver = true;
+    }
+
+    //public void RestartButtonClick()
+    //{
+    //    SceneManager.LoadScene("scene1");
+    //    //SceneManager.LoadScene(0);
+    //}
+}
